3a19a244c8a734ee6ab8abc5388bfa967e92ec39
Ta-Seen Islam <tahscenery@gmail.com>
2022-03-14 21:11:25 +1100

    Update deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+dotenv.config();
 
 import { HardhatUserConfig, task } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers';
@@ -8,7 +9,7 @@ import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 
-dotenv.config();
+const { ALCHEMY_API_KEY = '', PRIVATE_KEY = '' } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,30 +20,25 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
   }
 });
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
   networks: {
-    // ropsten: {
-    //   url: process.env.ROPSTEN_URL || '',
-    //   accounts:
-    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    // },
     rinkeby: {
-      url: process.env.RINKEBY_URL || '',
-      accounts: {
-        mnemonic: process.env.RINKEBY_MNEMONICS,
-      },
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: 'USD',
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,23 @@
 import { ethers } from 'hardhat';
 
+const { METADATA_CID } = process.env;
+
 async function main() {
+  if (!METADATA_CID) {
+    throw new Error(
+      'Metadata CID is not set. Please set one in an .env file before ' +
+        'deploying the contract.',
+    );
+  }
+
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contract with account:', deployer.address);
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+
   const HypeHaus = await ethers.getContractFactory('HypeHaus');
-  const hypeHaus = await HypeHaus.deploy();
+  const hypeHaus = await HypeHaus.deploy(555, `ipfs://${METADATA_CID}/`);
   await hypeHaus.deployed();
+
   console.log('HyperHaus deployed to:', hypeHaus.address);
 }
 
