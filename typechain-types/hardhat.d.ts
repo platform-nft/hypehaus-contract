d98868717926910d7fab9e9fc7051ac9df6b1fb4
Ta-Seen Islam <tahscenery@gmail.com>
2022-03-18 21:08:51 +1100

    Remove `HausCoin` contract

--- typechain-types/hardhat.d.ts
+++ typechain-types/hardhat.d.ts
@@ -17,18 +17,6 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Ownable__factory>;
     getContractFactory(
-      name: "ERC20",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.ERC20__factory>;
-    getContractFactory(
-      name: "IERC20Metadata",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.IERC20Metadata__factory>;
-    getContractFactory(
-      name: "IERC20",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.IERC20__factory>;
-    getContractFactory(
       name: "ERC721",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC721__factory>;
@@ -57,14 +45,6 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC165__factory>;
     getContractFactory(
-      name: "HausCoin",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.HausCoin__factory>;
-    getContractFactory(
-      name: "IHausCoin",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.IHausCoin__factory>;
-    getContractFactory(
       name: "HypeHaus",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.HypeHaus__factory>;
@@ -74,21 +54,6 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Ownable>;
-    getContractAt(
-      name: "ERC20",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.ERC20>;
-    getContractAt(
-      name: "IERC20Metadata",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.IERC20Metadata>;
-    getContractAt(
-      name: "IERC20",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.IERC20>;
     getContractAt(
       name: "ERC721",
       address: string,
@@ -124,16 +89,6 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.IERC165>;
-    getContractAt(
-      name: "HausCoin",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.HausCoin>;
-    getContractAt(
-      name: "IHausCoin",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.IHausCoin>;
     getContractAt(
       name: "HypeHaus",
       address: string,
--- typechain-types/index.ts
+++ typechain-types/index.ts
@@ -2,9 +2,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Ownable } from "./Ownable";
-export type { ERC20 } from "./ERC20";
-export type { IERC20Metadata } from "./IERC20Metadata";
-export type { IERC20 } from "./IERC20";
 export type { ERC721 } from "./ERC721";
 export type { ERC721URIStorage } from "./ERC721URIStorage";
 export type { IERC721Metadata } from "./IERC721Metadata";
@@ -12,14 +9,9 @@ export type { IERC721 } from "./IERC721";
 export type { IERC721Receiver } from "./IERC721Receiver";
 export type { ERC165 } from "./ERC165";
 export type { IERC165 } from "./IERC165";
-export type { HausCoin } from "./HausCoin";
-export type { IHausCoin } from "./IHausCoin";
 export type { HypeHaus } from "./HypeHaus";
 
 export { Ownable__factory } from "./factories/Ownable__factory";
-export { ERC20__factory } from "./factories/ERC20__factory";
-export { IERC20Metadata__factory } from "./factories/IERC20Metadata__factory";
-export { IERC20__factory } from "./factories/IERC20__factory";
 export { ERC721__factory } from "./factories/ERC721__factory";
 export { ERC721URIStorage__factory } from "./factories/ERC721URIStorage__factory";
 export { IERC721Metadata__factory } from "./factories/IERC721Metadata__factory";
@@ -27,6 +19,4 @@ export { IERC721__factory } from "./factories/IERC721__factory";
 export { IERC721Receiver__factory } from "./factories/IERC721Receiver__factory";
 export { ERC165__factory } from "./factories/ERC165__factory";
 export { IERC165__factory } from "./factories/IERC165__factory";
-export { HausCoin__factory } from "./factories/HausCoin__factory";
-export { IHausCoin__factory } from "./factories/IHausCoin__factory";
 export { HypeHaus__factory } from "./factories/HypeHaus__factory";
