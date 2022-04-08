95cfd2650e9a8caa6f0118750e4bd332e0727bf1
Ta-Seen Islam <tahscenery@gmail.com>
2022-04-08 15:20:39 +1000

    Add basic home page

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      currentColor: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: colors.orange,
+    },
   },
   plugins: [],
 };
